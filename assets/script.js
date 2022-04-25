cb9e75ac0e02d5219cb580a198a61cf8a29ebcea
Robert Routhier <robrouth92@gmail.com>
2022-04-25 12:48:30 -0700

    some styling up

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
 function getWeather(city){
     var oneCall = 'https://api.openweathermap.org/data/2.5/onecall';
     var API_KEY = '4253ae682bded8fe54667e18d996e279';
-    var weatherBase = 'https://api.openweathmap.org/data/2.5/weather'
-    fetch(weatherBase + `?q=${endcodeURI(city)}&appid=${API_KEY}`)
+    var weatherBase = 'https://api.openweathermap.org/data/2.5/weather';
+    var city =
+    fetch(weatherBase + `?q=${encodeURI(city)}&appid=${API_KEY}`)
     .then(weatherRes => weatherRes.json())
     .then(weatherData =>{
+        console.log(weatherData)
         var lat = weatherData.coord.lat;
         var lon = weatherData.coord.lon;
         fetch(oneCall + `?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
         .then(oneCallRes =>oneCallRes.json())
         .then(oneCallData =>{
         var currentWeather = oneCallData.current.weather[0].main
-        document.querySelector(#city-weather).textContent = currentWeather
+        document.querySelector('#city-weather').textContent = currentWeather
         console.log(oneCallData)
         })
     })
