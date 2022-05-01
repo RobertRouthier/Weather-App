6c1422c5898e971c35267735fcad91abb32fcbf6
Robert Routhier <robrouth92@gmail.com>
2022-05-01 15:53:30 -0700

    got first day forecast done

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ function getWeather(city){
     var oneCall = 'https://api.openweathermap.org/data/2.5/onecall';
     var API_KEY = '4253ae682bded8fe54667e18d996e279';
     var weatherBase = 'https://api.openweathermap.org/data/2.5/weather';
+    var fiveDayBase = 'https://api.openweathermap.org/data/2.5/forecast'
     var city =
     fetch(weatherBase + `?q=${encodeURI(city)}&appid=${API_KEY}`)
     .then(weatherRes => weatherRes.json())
@@ -16,8 +17,37 @@ function getWeather(city){
         document.querySelector('.description').textContent = currentWeather
         console.log(oneCallData)
         oneCallData.name = weatherData.name;
-        postWeather(oneCallData)
+        fetch(fiveDayBase + `?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`)
+        .then(fiveDayRes => fiveDayRes.json())
+        .then(fiveDayData =>{
+            //var singleCard = document.querySelectorAll('single-card')
+            for (var i = 0; i < 5; i++){
+                var fiveIcon = fiveDayData.list[i].weather[0].icon
+                document.querySelector('.five-icon').innerHTML = fiveIcon
+            }
+            for (var i = 0; i < 5; i++){
+                var fiveTemp = fiveDayData.list[i].main.temp
+                document.querySelector('.five-temp').innerHTML = fiveTemp + ' ℉'
+            }
+            for (var i = 0; i < 5; i++){
+                var fiveWeather = fiveDayData.list[i].weather[0].main
+                document.querySelector('.five-description').innerHTML = fiveWeather
+            }
+            for (var i = 0; i < 5; i++){
+                var fiveHumid = fiveDayData.list[i].main.humidity
+                document.querySelector('.five-humidity').innerHTML = fiveHumid + '%'
+            }
+            for (var i = 0; i < 5; i++){
+                var fiveWind = fiveDayData.list[i].wind.speed
+                document.querySelector('.five-wind').innerHTML = fiveWind + " mph"
+            }
+        
+        console.log(fiveDayData)
+        console.log(fiveIcon, fiveTemp, fiveWeather, fiveHumid, fiveWind)
     })
+    postWeather(oneCallData)
+    //postFiveDay(fiveDayData)
+})
 })
 }
 
@@ -33,7 +63,7 @@ document.querySelector('.search').addEventListener('keyup', function(event){
     }
 })
 
-//read documentation to convert from imperial to faranheit
+
 
 function postWeather(oneCallData){
     
@@ -52,6 +82,19 @@ function postWeather(oneCallData){
     document.querySelector('.wind').innerHTML = 'Wind Speed ' + wind + " mph"
     
     
+}
+
+function postFiveDay(fiveDayData){
+    var name = oneCallData.name
+    var icon= oneCallData.current.weather[0].icon
+    var temp = 'Temp ' + oneCallData.current.temp + ' ℉'
+    var wind = oneCallData.current.wind_speed
+    var humid = 'Humidity ' + oneCallData.current.humidity + '%'
+
+    document.querySelector('.five-icon').innerHTML = icon
+    document.querySelector('.five-temp').innerHTML = ' ' + temp 
+    document.querySelector('.humidity').innerHTML =  humid
+    document.querySelector('.wind').innerHTML = 'Wind Speed ' + wind + " mph"
 }
 
 //figure out 5 day forecast
